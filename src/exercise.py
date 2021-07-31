f437b3ce7a6bebda07a56cc78a1bc8b21b79a7d0
Justin Turner <jmturner638@gmail.com>
2021-07-31 01:13:06 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    names = []
+
+    while True:
+        name = input()
+
+        if name == '':
+            break
+
+        names.append(name)
+
+    print("In total:", len(names))
 
 if __name__ == '__main__':
     main()
